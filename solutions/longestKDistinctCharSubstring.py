ccf1ffcd10841046d32172d14d4ce532bbc7e128
ckallum <ckallumke@gmail.com>
2019-08-26 02:49:27 +0100

    Done

--- solutions/longestKDistinctCharSubstring.py
+++ solutions/longestKDistinctCharSubstring.py
@@ -1,35 +1,35 @@
 from collections import deque
 
 
-def find(sequence, uniquenums):
-    currentsequence = deque()
-    currentmax = 0
-    valueDict = dict()
-    for x in sequence:
-        valueDict[x] = 0
-    for x in sequence:
-        currentsequence.append(x)
-        valueDict[x] += 1
-        while notValid(valueDict, uniquenums):
-            val = currentsequence.popleft()
-            valueDict[val] -= 1
-        if len(currentsequence) > currentmax:
-            currentmax = len(currentsequence)
-    return currentmax
+def longest_substring_with_k_distinct_characters(string, k):
+    char_dict = dict()
+    current_sub_string = deque()
+    largest_substring = ""
+    for char in string:
+        current_sub_string.append(char)
+        try:
+            char_dict[char] += 1
+        except:
+            char_dict[char] = 1
+        while not is_valid(char_dict, k):
+            char_dict[current_sub_string.popleft()] -= 1
+        if len(current_sub_string) > len(largest_substring):
+            largest_substring = "".join(list(current_sub_string))
+    return largest_substring
 
 
-def notValid(diction, uniquenums):
+def is_valid(char_count, k):
     count = 0
-    for x in diction:
-        if diction[x] > 0:
+    for char in char_count:
+        if char_count[char] > 0:
             count += 1
-            if count > uniquenums:
-                return True
-    return False
+    if count > k:
+        return False
+    return True
 
 
 def main():
-    assert find([1, 3, 5, 3, 1, 3, 1, 5], 2) == 4
+    assert longest_substring_with_k_distinct_characters("aabcdefff", 3) == "defff"
 
 
 if __name__ == '__main__':
