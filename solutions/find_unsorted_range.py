0135fa383f906a82d7dee28c1a13fd18d78ecd9f
ckallum <ckallumke@gmail.com>
2019-09-16 13:39:36 +0100

    Done

--- solutions/find_unsorted_range.py
+++ solutions/find_unsorted_range.py
@@ -2,24 +2,29 @@ from cmath import inf
 
 
 def find_range(nums):
-    start_index = 0
-    end_index = 0
+    start_index = -1
+    end_index = -1
     current_max = -inf
     needs_sorted = False
     for index, number in enumerate(nums):
-        if not needs_sorted and number >= nums[start_index]:
-            start_index = index
         if number >= current_max:
             current_max = number
         else:
             end_index = index
-            needs_sorted = True
+            if not needs_sorted:
+                start_index = 0
+                while number > nums[start_index]:
+                    start_index += 1
+                needs_sorted = True
 
+    print(start_index, end_index)
     return start_index, end_index
 
 
 def main():
     assert find_range([1, 7, 5, 9, 7, 8, 10]) == (1, 5)
+    assert find_range([1, 2, 3, 4, 5, 6, 1]) == (0, 6)
+    assert find_range([1, 2, 3, 4, 5]) == (-1, -1)
 
 
 if __name__ == '__main__':
