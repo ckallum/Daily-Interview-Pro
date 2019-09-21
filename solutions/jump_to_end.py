e7e5a710d8378116e64b84911234173f972d82ec
ckallum <ckallumke@gmail.com>
2019-09-21 16:51:10 +0100

    Done

--- solutions/jump_to_end.py
+++ solutions/jump_to_end.py
@@ -6,6 +6,8 @@ def jump_to_end_back_to_front(jumps):
     for i in range(len(jumps) - 2, -1, -1):
         if jumps[i] >= len(jumps) - i - 1:
             jump_dict[i] = 1
+        elif jumps[i] == 0:
+            jump_dict[i] = inf
         else:
             min_jump = inf
             for j in range(1, jumps[i] + 1):
@@ -17,7 +19,7 @@ def jump_to_end_back_to_front(jumps):
 
 
 def main():
-    assert jump_to_end_back_to_front([1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]) == 3
+    assert jump_to_end_back_to_front([1, 3, 0, 8, 9, 2, 6, 7, 6, 8, 9]) == 3
     assert jump_to_end_back_to_front([3, 2, 5, 1, 1, 9, 3, 4]) == 2
 
 
