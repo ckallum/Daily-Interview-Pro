64e4a6a48d7bde0be9ea0bb90e2943c1fdc84fb1
ckallum <ckallumke@gmail.com>
2019-10-05 17:15:50 +0100

    Done

---
+++ solutions/euler_walk.py
@@ -0,0 +1,18 @@
+class Graph(object):
+    pass
+
+
+def euler_helper(current_node, edges, visited):
+    pass
+
+
+def is_euler(edges):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
